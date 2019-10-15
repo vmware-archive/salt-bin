125154a744ab0e33c6afc83d62d36a0ddd08579e
Thomas S Hatch <thatch45@gmail.com>
2019-10-15 10:51:34 -0600

    we make repos now :)

--- mk_repo.py
+++ mk_repo.py
@@ -11,6 +11,8 @@ from distutils.version import StrictVersion
 # Import third party libs
 import msgpack
 
+LATEST = 'salt'
+
 
 def scan():
     data = {}
@@ -18,37 +20,30 @@ def scan():
         if root == 'dist':
             continue
         base = os.path.basename(root)
-        data[base] = []
+        data[base] = {}
         for fn in files:
-            if fn.endswith(('sha512', 'blake', 'salt', 'sha3_512')):
+            if fn == LATEST:
                 continue
-            data[base].append(fn)
-    for root, dirs, files in os.walk('dist'):
-        if root == 'dist':
-            continue
-        for fn in files:
+            data[base][fn] = {
+                    'name': fn,
+                    'version': fn[fn.index('-')+1:]
+                    }
             full = os.path.join(root, fn)
-            sha512_p = f'{full}.sha512'
-            sha_3_512_p = f'{full}.sha3_512'
-            blake_p = f'{full}.blake'
             with open(full, 'rb') as rfh:
                 raw = rfh.read()
-                blake_digest = hashlib.blake2b(raw).hexdigest()
-                sha512_digest = hashlib.sha512(raw).hexdigest()
-                sha_3_512_digest = hashlib.sha3_512(raw).hexdigest()
-            with open(sha512_p, 'w+') as wfh:
-                wfh.write(sha512_digest)
-            with open(sha_3_512_p, 'w+') as wfh:
-                wfh.write(sha_3_512_digest)
-            with open(blake_p, 'w+') as wfh:
-                wfh.write(blake_digest)
+                data[base][fn]['blake'] = hashlib.blake2b(raw).hexdigest()
+                data[base][fn]['sha512'] = hashlib.sha512(raw).hexdigest()
+                data[base][fn]['sha3_512'] = hashlib.sha3_512(raw).hexdigest()
+
     for base, rels in data.items():
         names = []
-        for rel in rels:
-            names.append(rel[rel.index('-')+1:])
+        for fn, rel in rels.items():
+            names.append(rel['version'])
         names = sorted(names, key=StrictVersion)
         latest = os.path.join(f'salt-{names[-1]}')
         latest_lk = os.path.join('dist', base, 'salt')
+        if os.path.exists(latest_lk):
+            os.remove(latest_lk)
         os.symlink(latest, latest_lk)
     with open('dist/repo.mp', 'wb+') as wfh:
         wfh.write(msgpack.dumps(data))
