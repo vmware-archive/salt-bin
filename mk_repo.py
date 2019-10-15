0e9bf924115904af0c5bf1a5493772745b0cbef0
Thomas S Hatch <thatch45@gmail.com>
2019-10-15 10:20:04 -0600

    Add checksums to the repo.
    This also adds a latest link called salt on a per os basis

--- mk_repo.py
+++ mk_repo.py
@@ -5,6 +5,7 @@ Create the repo files used by heist to read what files are available
 import os
 import json
 import pprint
+import hashlib
 from distutils.version import StrictVersion
 
 # Import third party libs
@@ -19,14 +20,35 @@ def scan():
         base = os.path.basename(root)
         data[base] = []
         for fn in files:
+            if fn.endswith(('sha512', 'blake', 'salt', 'sha3_512')):
+                continue
             data[base].append(fn)
+    for root, dirs, files in os.walk('dist'):
+        if root == 'dist':
+            continue
+        for fn in files:
+            full = os.path.join(root, fn)
+            sha512_p = f'{full}.sha512'
+            sha_3_512_p = f'{full}.sha3_512'
+            blake_p = f'{full}.blake'
+            with open(full, 'rb') as rfh:
+                raw = rfh.read()
+                blake_digest = hashlib.blake2b(raw).hexdigest()
+                sha512_digest = hashlib.sha512(raw).hexdigest()
+                sha_3_512_digest = hashlib.sha3_512(raw).hexdigest()
+            with open(sha512_p, 'w+') as wfh:
+                wfh.write(sha512_digest)
+            with open(sha_3_512_p, 'w+') as wfh:
+                wfh.write(sha_3_512_digest)
+            with open(blake_p, 'w+') as wfh:
+                wfh.write(blake_digest)
     for base, rels in data.items():
         names = []
         for rel in rels:
             names.append(rel[rel.index('-')+1:])
         names = sorted(names, key=StrictVersion)
         latest = os.path.join(f'salt-{names[-1]}')
-        latest_lk = os.path.join('dist', base, 'latest')
+        latest_lk = os.path.join('dist', base, 'salt')
         os.symlink(latest, latest_lk)
     with open('dist/repo.mp', 'wb+') as wfh:
         wfh.write(msgpack.dumps(data))
