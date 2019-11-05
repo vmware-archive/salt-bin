bd06dce19329346bdf8ef5a9aeb0366c515aa842
Pedro Algarvio <pedro@algarvio.me>
2019-11-05 05:15:29 +0000

    Exit script on errors. Update SSL certs. Try insecure curl as a last resort

--- scripts/cent6_setup.sh
+++ scripts/cent6_setup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
+#Exit on Errors
+set -e
+
 PYVER='3.7.4'
 PYPRE='3.7'
 
 yum install -y xz libffi-devel bzip2-devel xz-devel ncurses-devel gdbm-devel sqlite-devel readline-devel zlib-devel libuuid-devel
 yum groupinstall -y 'Development Tools'
 
-curl -LO  https://www.openssl.org/source/openssl-1.1.1d.tar.gz
+# Update SSL Certificates
+curl http://curl.haxx.se/ca/cacert.pem -o /etc/pki/tls/certs/ca-bundle.crt
+update-ca-trust check
+update-ca-trust enable
+
+curl -LO  https://www.openssl.org/source/openssl-1.1.1d.tar.gz || curl -k -LO https://www.openssl.org/source/openssl-1.1.1d.tar.gz
 tar -xvf openssl-1.1.1d.tar.gz
 
 cd openssl-1.1.1d
@@ -26,3 +35,7 @@ LD_LIBRARY_PATH=/usr/local/python37/lib/:/usr/local/openssl11/lib/ pip3 install 
 ln -s /usr/local/python37/bin/staticx /usr/local/bin/staticx
 cd /opt
 LD_LIBRARY_PATH=/usr/local/python37/lib/:/usr/local/openssl11/lib/ python3.7 build.py -S
+
+if [ "${EXT_UID}" != "" -a "${EXT_GID}" != "" ]; then
+    chown -R ${EXT_UID}:${EXT_GID} /opt
+fi
