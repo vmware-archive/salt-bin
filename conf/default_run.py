c27cb9f554de9e9d1b90de9586bd40ef86fccf29
Thomas S Hatch <thatch45@gmail.com>
2020-04-09 15:24:26 -0600

    enable debug python shell and pip

--- conf/default_run.py
+++ conf/default_run.py
@@ -3,11 +3,16 @@
 
 # Import python libs
 import sys
+import os
 import multiprocessing
 
 # Import salt libs
 import salt.scripts
 import salt.utils.platform
+
+# Import third party libs
+from pip._internal.cli.main import main
+
 
 AVAIL = (
         'minion',
@@ -19,7 +24,9 @@ AVAIL = (
         'extend',
         'key',
         'proxy',
+        'pip',
         'run',
+        'shell',
         'ssh',
         'support',
         'syndic',
@@ -37,7 +44,13 @@ def redirect():
         print(msg)
         sys.exit(1)
     cmd = sys.argv[1]
-    if cmd not in AVAIL:
+    if cmd == "shell":
+        py_shell()
+        return
+    elif cmd == "pip":
+        pip()
+        return
+    elif cmd not in AVAIL:
         # Fall back to the salt command
         sys.argv[0] = 'salt'
         s_fun = salt.scripts.salt_main
@@ -46,6 +59,24 @@ def redirect():
         sys.argv.pop(1)
         s_fun = getattr(salt.scripts, 'salt_{0}'.format(cmd))
     s_fun()
+
+
+def py_shell():
+    import readline # optional, will allow Up/Down/History in the console
+    import code
+    variables = globals().copy()
+    variables.update(locals())
+    shell = code.InteractiveConsole(variables)
+    shell.interact()
+
+
+def pip():
+    cmd = sys.argv[2]
+    args = [cmd, '--target', sys._MEIPASS,]
+    args.extend(sys.argv[3:])
+    parser = ['pip'] + args
+    sys.argv = parser
+    main(args)
 
 
 if __name__ == '__main__':
